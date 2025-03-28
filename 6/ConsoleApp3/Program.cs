fbfe20f16bd6861e72aee29795d65e665e25b657
Makc222021 <mister.makc222@mail.ru>
2025-03-28 17:53:25 +0300

    pr_6

---
+++ 6/ConsoleApp3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+// Класс для обработки данных
+class DataProcessor
+{
+    // Событие завершения обработки данных
+    public event EventHandler ProcessingCompleted;
+
+    // Метод для обработки данных и генерации события
+    public void ProcessData()
+    {
+        Console.WriteLine("Обработка данных...");
+
+        // Предположим, здесь происходит обработка данных
+
+        // Генерируем событие о завершении обработки данных
+        OnProcessingCompleted();
+    }
+
+    // Метод для генерации события ProcessingCompleted
+    protected virtual void OnProcessingCompleted()
+    {
+        ProcessingCompleted?.Invoke(this, EventArgs.Empty);
+    }
+}
+
+// Класс для создания отчета
+class ReportGenerator
+{
+    // Метод, подписанный на событие ProcessingCompleted
+    public void GenerateReport(object sender, EventArgs e)
+    {
+        Console.WriteLine("Создание отчета...");
+    }
+}
+
+// Класс для оповещения пользователя
+class Notifier
+{
+    // Метод, подписанный на событие ProcessingCompleted
+    public void NotifyUser(object sender, EventArgs e)
+    {
+        Console.WriteLine("Пользователь оповещен о завершении обработки данных.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DataProcessor processor = new DataProcessor();
+        ReportGenerator reportGenerator = new ReportGenerator();
+        Notifier notifier = new Notifier();
+
+        // Подписываемся на событие ProcessingCompleted
+        processor.ProcessingCompleted += reportGenerator.GenerateReport;
+        processor.ProcessingCompleted += notifier.NotifyUser;
+
+        // Запускаем обработку данных
+        processor.ProcessData();
+
+        Console.ReadLine();
+    }
+}
---
+++ 6/ConsoleApp4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+// Класс для отслеживания загрузки файлов
+class FileDownloader
+{
+    // Событие изменения прогресса загрузки
+    public event EventHandler<int> DownloadProgressChanged;
+
+    // Метод для имитации загрузки файла
+    public void DownloadFile()
+    {
+        for (int i = 0; i <= 100; i += 10)
+        {
+            OnDownloadProgressChanged(i);
+            System.Threading.Thread.Sleep(500); // Задержка для имитации загрузки
+        }
+    }
+
+    // Метод для генерации события DownloadProgressChanged
+    protected virtual void OnDownloadProgressChanged(int progress)
+    {
+        DownloadProgressChanged?.Invoke(this, progress);
+    }
+}
+
+// Промежуточный класс для мониторинга загрузки
+class DownloadMonitor
+{
+    // Метод подписывает ProgressBar на событие DownloadProgressChanged
+    public void UpdateProgressBar(object sender, int progress)
+    {
+        Console.WriteLine($"Прогресс загрузки: {progress}%");
+    }
+
+    // Метод подписывает Logger на событие DownloadProgressChanged
+    public void LogProgress(object sender, int progress)
+    {
+        Console.WriteLine($"Лог: Загрузка на {progress}% завершена");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        FileDownloader downloader = new FileDownloader();
+        DownloadMonitor monitor = new DownloadMonitor();
+
+        // Подписываем ProgressBar и Logger на событие DownloadProgressChanged
+        downloader.DownloadProgressChanged += monitor.UpdateProgressBar;
+        downloader.DownloadProgressChanged += monitor.LogProgress;
+
+        // Запускаем загрузку файла
+        downloader.DownloadFile();
+
+        Console.ReadLine();
+    }
+}
